8f97ea324e76c2a680d12c9dc0a264716bc834d0
Miguel Ceballos <mceballosleonides@gmail.com>
2024-07-22 16:48:59 -0600

    Modifica las migraciones

--- database/migrations/2024_07_22_216617_create_tasks_table.php
+++ database/migrations/2024_07_22_216617_create_tasks_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->string('status');
             $table->timestamps();
         });
