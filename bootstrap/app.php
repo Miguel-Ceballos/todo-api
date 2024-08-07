4aff87cc6a7472653f9645fa885dc8fdb893b521
Miguel Ceballos <mceballosleonides@gmail.com>
2024-08-07 14:42:52 -0600

    feat(handling errors): personaliza el mensaje recibido al no encontrar una categoría.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,5 +21,13 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function(NotFoundHttpException $e, $request) {
+            return response()->json([
+                'errors' => [
+                    'code' => $e->getStatusCode(),
+                    'type' => 'NotFoundHttpException',
+                    'message' => $e->getMessage(),
+                ]
+            ]);
+        });
     })->create();
