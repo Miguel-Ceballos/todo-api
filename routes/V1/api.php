6f5ef56117b9559acc4226ba6ceb15abc5d21ac9
Miguel Ceballos <mceballosleonides@gmail.com>
2024-08-07 12:10:02 -0600

    docs: agrupa rutas por middleware

--- routes/V1/api.php
+++ routes/V1/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Api\V1\TaskController;
 use App\Http\Controllers\AuthorController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('tasks', TaskController::class);
+});
 
-Route::middleware('auth:sanctum')->apiResource('categories', CategoryController::class);
-Route::middleware('auth:sanctum')->apiResource('tasks', TaskController::class);
