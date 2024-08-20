4619b98743b4854d37fd432f6a81493b62c1b7a2
Miguel Ceballos <mceballosleonides@gmail.com>
2024-08-20 13:35:18 -0600

    feat(api-filters): agrega filtrado de tareas por titulo, descripción, createdAt y updatedAt

--- app/Http/Filters/V1/TaskFilter.php
+++ app/Http/Filters/V1/TaskFilter.php
@@ -6,6 +6,36 @@ use Illuminate\Support\Facades\Auth;
 
 class TaskFilter extends QueryFilter
 {
+    public function updatedAt($value)
+    {
+        $dates = explode(',', $value);
+        if ( count($dates) > 1 ) {
+            return $this->builder->whereBetween('updated_at', $dates);
+        }
+        return $this->builder->whereDate('updated_at', $value);
+    }
+
+    public function createdAt($value)
+    {
+        $dates = explode(',', $value);
+        if ( count($dates) > 1 ) {
+            return $this->builder->whereBetween('created_at', $dates);
+        }
+        return $this->builder->whereDate('created_at', $value);
+    }
+
+    public function description($value)
+    {
+        $like_str = str_replace('*', '%', $value);
+        return $this->builder->where('user_id', Auth::user()->id)->where('description', 'like', $like_str);
+    }
+
+    public function title($value)
+    {
+        $like_str = str_replace('*', '%', $value);
+        return $this->builder->where('user_id', Auth::user()->id)->where('title', 'like', $like_str);
+    }
+
     public function status($value)
     {
         return $this->builder->where('user_id', Auth::user()->id)->where('status', $value);
