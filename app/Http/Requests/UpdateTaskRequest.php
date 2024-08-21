6ed80859ec908cdd3a3a1ef3304f6d714b4fb9ef
Miguel Ceballos <mceballosleonides@gmail.com>
2024-08-20 22:11:17 -0600

    refactor(request): cambia en el request los valores requeridos a 'sometimes' para no generar problemas al actualizar una tarea.

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class UpdateTaskRequest extends FormRequest
+class UpdateTaskRequest extends BaseTaskRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,11 +22,10 @@ class UpdateTaskRequest extends FormRequest
         $user_categories = $this->user()->categories;
         $categories = implode(',', $user_categories->pluck('id')->toArray());
         return [
-            'data.relationships.user.data.id' => 'prohibited',
-            'data.relationships.category.data.id' => 'required|integer|in:' . $categories,
-            'data.attributes.title' => 'required|string|max:255',
-            'data.attributes.description' => 'string|max:500',
-            'data.attributes.status' => 'required|string|in:C,D,P',
+            'data.relationships.category.data.id' => 'sometimes|integer|in:' . $categories,
+            'data.attributes.title' => 'sometimes|string|max:255',
+            'data.attributes.description' => 'sometimes|string|max:500',
+            'data.attributes.status' => 'sometimes|string|in:C,D,P',
         ];
     }
 }
