4c709311114477d10f614743101f8fbbaf13a914
Miguel Ceballos <mceballosleonides@gmail.com>
2024-09-15 16:33:54 -0600

    fix(categories): Soluciona error que no permitia a otros usuarios agregar una categoria ya agregada por otro usuario, validando que un usuario no agregue la misma categoria dos veces

--- app/Http/Requests/StoreCategoryRequest.php
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Category;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class StoreCategoryRequest extends BaseCategoryRequest
 {
@@ -21,9 +25,26 @@ class StoreCategoryRequest extends BaseCategoryRequest
      */
     public function rules(): array
     {
+        $isCategoryExists = Category::where('slug', Str::slug($this->input('data.attributes.title')))
+            ->where('user_id', Auth::user()->id)
+            ->exists();
+        if ($isCategoryExists) {
+            return [
+                'data.attributes.title' => 'required|string|max:50|unique:categories,title',
+                'data.attributes.slug' => 'string|unique:categories,slug',
+            ];
+        }
+
         return [
             'data.attributes.title' => 'required|string|max:50',
             'data.attributes.slug' => 'string',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'data.attributes.title.unique' => 'The category already exists',
         ];
     }
 }
