b553578543f9162ed0f0d4485faa6df96efbfe28
Miguel Ceballos <mceballosleonides@gmail.com>
2024-09-23 13:44:58 -0600

    docs(tasks): documenta funciones que manipulan las tareas por categoría

--- app/Http/Controllers/Api/V1/CategoryTasksController.php
+++ app/Http/Controllers/Api/V1/CategoryTasksController.php
@@ -19,7 +19,13 @@ class CategoryTasksController extends ApiController
     protected $policyClass = CategoryTaskPolicy::class;
 
     /**
-     * Display a listing of the resource.
+     * Get all tasks
+     *
+     * @group Managing Tasks by Category
+     * @queryParam sort string Data field(s) to sort by. Multiple fields can be specified by separating them with a comma. Denote descending sort with a minis (-) sign. Example: sort=-created_at,title
+     * @queryParam filter[status] Filter by status code: C: completed, D: Doing, P: Pending. No-example
+     * @queryParam filter[title] Filter by title. Wildcards ares supported. Example: filter[title]=*test*
+     *
      */
     public function index(Category $category, TaskFilter $filters)
     {
@@ -32,7 +38,42 @@ class CategoryTasksController extends ApiController
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Create a task
+     *
+     * Creates a new task for the authenticated user.
+     *
+     * @group Managing Tasks by Category
+     * @response 201 {
+     * "data": {
+     *  "type": "task",
+     *  "id": 602,
+     *  "attributes": {
+     *      "title": "Task test",
+     *      "description": "Task description",
+     *      "status": "P",
+     *      "created_at": "2024-09-23T17:16:52.000000Z",
+     *      "updated_at": "2024-09-23T17:16:52.000000Z"
+     *  },
+     *  "relationships": {
+     *      "user": {
+     *          "data": {
+     *              "type": "user",
+     *              "id": 2
+     *          }
+     *      },
+     *      "category": {
+     *          "data": {
+     *              "type": "category",
+     *              "id": 1
+     *          }
+     *      }
+     *  },
+     *  "links": {
+     *      "self": "http://localhost/api/v1/tasks/602"
+     *  },
+     * },
+     * }
+     *
      */
     public function store(Category $category, StoreTaskRequest $request)
     {
@@ -43,7 +84,42 @@ class CategoryTasksController extends ApiController
     }
 
     /**
-     * Display the specified resource.
+     * Show a task
+     *
+     * Displays the specified task.
+     *
+     * @group Managing Tasks by Category
+     * @response 200 {
+     * "data": {
+     * "type": "task",
+     * "id": 5,
+     * "attributes": {
+     * "title": "quis repudiandae",
+     * "description": "Sed pariatur earum atque harum porro optio reprehenderit.",
+     * "status": "C",
+     * "created_at": "2024-09-15T18:43:54.000000Z",
+     * "updated_at": "2024-09-19T01:31:30.000000Z"
+     * },
+     * "relationships": {
+     * "user": {
+     * "data": {
+     * "type": "user",
+     * "id": 2
+     * }
+     * },
+     * "category": {
+     * "data": {
+     * "type": "category",
+     * "id": 5
+     * }
+     * }
+     * },
+     * "links": {
+     * "self": "http://localhost/api/v1/tasks/5"
+     * }
+     * }
+     * }
+     *
      */
     public function show(Category $category, $task_id)
     {
@@ -57,7 +133,42 @@ class CategoryTasksController extends ApiController
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update a task
+     *
+     * update the specified task.
+     *
+     * @group Managing Tasks by Category
+     * @response 200 {
+     * "data": {
+     *  "type": "task",
+     *  "id": 602,
+     *  "attributes": {
+     *      "title": "Task test updated",
+     *      "description": "Task description updated",
+     *      "status": "P",
+     *      "created_at": "2024-09-23T17:16:52.000000Z",
+     *      "updated_at": "2024-09-23T17:16:52.000000Z"
+     *  },
+     *  "relationships": {
+     *      "user": {
+     *          "data": {
+     *              "type": "user",
+     *              "id": 2
+     *          }
+     *      },
+     *      "category": {
+     *          "data": {
+     *              "type": "category",
+     *              "id": 1
+     *          }
+     *      }
+     *  },
+     *  "links": {
+     *      "self": "http://localhost/api/v1/tasks/602"
+     *  },
+     * },
+     * }
+     *
      */
     public function update(Category $category, $task_id, UpdateTaskRequest $request)
     {
@@ -72,7 +183,17 @@ class CategoryTasksController extends ApiController
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Delete a task
+     *
+     * Delete the specified task.
+     *
+     * @group Managing Tasks by Category
+     * @response 200{
+     *     "data": [],
+     *     "message": "Task deleted successfully",
+     *     "status": 200
+     * }
+     *
      */
     public function destroy(Category $category, $task_id)
     {
